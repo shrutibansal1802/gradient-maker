e3a0afeb4bd72bf18d9426e18590b0dd53f2879d
shruti <shrutibansal1802@gmail.com>
2020-07-24 04:12:57 +0530

    basic gradient-maker

--- script.js
+++ script.js
@@ -1,42 +1,11 @@
-const root = document.documentElement
-const theme = document.querySelectorAll('.theme > button')
-const inputs =document.querySelectorAll('.color-box > input')
+root =document.documentElement;
+const inputs=Array.from( document.querySelectorAll(".input-box input"))
 
-inputs.forEach(input => {
-    input.addEventListener('input',updatecolor)
-})
-function updatecolor(e)
-{
-    let value = e.target.value
-    var inputid =e.target.id
-    var inputbg = `--bg-${inputid}`
-    root.style.setProperty(inputbg, value)
+inputs.forEach(input => input.addEventListener('change',updatebg))
+
+function updatebg(){
+    const color1 =this.value;
+    console.log(color1)
+    document.documentElement.style.setProperty(`--${this.id}`,this.value)
+    
 }
-
-theme.forEach((btn)=>{
-    btn.addEventListener('click', handletheme)
-})
-function handletheme(e){
-    switch(e.target.value)
-    {
-        case 'dark':
-            root.style.setProperty('--bgcolor','black')
-            root.style.setProperty('--fcolor','white')
-            break;
-        case 'light':
-            root.style.setProperty('--bgcolor','white')
-            root.style.setProperty('--fcolor','black')
-            break;
-        case 'calm':
-            root.style.setProperty('--bgcolor','skyblue')
-            root.style.setProperty('--fcolor','black')
-            break;
-    }
-}
-// var range =document.querySelector('.booth-slider')
-
-function getrange(val)
-{
-    root.style.setProperty('--slider',val)
-}
-
